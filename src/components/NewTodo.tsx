1f5bbe1fa5371ed4bccf46328e16aa9a1b0c62a3
L1ndseyHerman <lherman21@jcu.edu>
2021-10-12 14:53:36 -0400

    Caught up to vid409: TypeScript events, refs, and the dif btw "?" and "!".

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewTodo = () => {
+  //  Refs need types in TypeScript. This is the right type for this one
+  //  It also needs a starting value of null.
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
   //  This is the correct type!
   //  If you did an onclick, would be React.MouseEvent!
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    //  The ref might not be set to a value yet when u use it, so it needs a "?".
+    //const enteredText = todoTextInputRef.current?.value;
+
+    //  If u know that a value that could possibly be null won't be, like when u submit
+    //  a form, it will definitely have something in it, can replace "?" with "!".
+
+    //  "?" and "!" are TypeScript-specific things that deal w values that could be null.
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      //  throw an error
+      return;
+    }
   };
   //  Don't forget the "htmlFor" and the "id" for accessability!
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
