2b24e78010fd09d455458e4ded37d3d2b59cc7c8
L1ndseyHerman <lherman21@jcu.edu>
2021-10-12 14:24:26 -0400

    Caught up to vid407: creating a second typed component.

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 //  If u import a class file, it can be a type!
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 //  React.FC is a pre-coded TypeScript type! It has props.children for like <Wrapper />s n stuff!
 //  Define your own props in the <>, and it will merge with the existing ones!
@@ -9,7 +10,7 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
