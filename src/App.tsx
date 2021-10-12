128df2fcc1c9084b48ce8793263fdb3c1629d942
L1ndseyHerman <lherman21@jcu.edu>
2021-10-12 15:24:35 -0400

    Caught up to vid411: how to add TypeScript typing to a useState() so that u don't get a type of "never[]" aka, u can never add values to the array!

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  //  Ahh, and here's that "never[]" type that I struggled with in the other
+  //  programming knowledge test!
+  //  "never[]" means no values are ever allowed in the array?! When would u ever want that?!
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) => {};
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
